0dd82f0b70ab04165f05b683e66ef5028da928d0
itdyid2-blip <itdyid2@gmail.com>
2026-01-13 10:13:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("btn").addEventListener("click", function() {
+    alert("Biz bilan bog‘lanish uchun pastga tushing 👇");
+  });
+  
+  function sendMessage() {
+    document.getElementById("msg").innerText = "Rahmat! Tez orada siz bilan bog‘lanamiz.";
+  }
+  
